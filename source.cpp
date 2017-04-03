3c592f81e8b5735d37f29530e259e1ded345707d
h2kmagicman <vntrdvll@gmail.com>
2017-04-03 16:47:44 -0500

    Main CPP File

---
+++ source.cpp
@@ -0,0 +1,34 @@
+//Endless Sea
+//V.0.1 WIP
+
+#include <iostream>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+//Character Variables
+int charsex
+int starthealth
+int startaccuracy
+
+
+
+int charcreate()
+{
+	cout << "Welcome to Endless Sea" << endl;
+	cout << "Let's begin by creating a cahracter."
+	
+}
+
+int main()
+{
+	cout << "Endless Sea" << endl;
+	cout << "Please Select From The Following Options: "
+	
+	
+	
+	int charcreate();
+
+	return 0;
+}
